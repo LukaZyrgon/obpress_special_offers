f6fa868a6c00b849fd6510d5f0c7df459f1b2198
LukaZyrgon <58442639+LukaZyrgon@users.noreply.github.com>
2022-02-28 15:54:22 +0100

    Update OBPress_SpecialOffers.php

--- OBPress_SpecialOffers.php
+++ OBPress_SpecialOffers.php
@@ -4,7 +4,7 @@
   Plugin uri: www.zyrgon.net
   Text Domain: OBPress_SpecialOffers
   Description: Widgets to OBPress
-  Version: 0.1.1
+  Version: 0.2.0
   Author: Zyrgon
   Author uri: www.zyrgon.net
   License: GPlv2 or Later
